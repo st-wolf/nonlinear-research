c59b18013f49612086192252c1938bc0a8a08c41
Mikhail Lebedev <m.lebedev@metalab.ifmo.ru>
2016-12-27 19:57:08 +0300

    Minor changes and figures

--- phase_transition/preparing_paper.m
+++ phase_transition/preparing_paper.m
@@ -50,3 +50,129 @@ plot(X, Phi_asymmetric(:, 1) + zero_level, '--', 'LineWidth', 2, 'Color', 'k');
 plot(X, zero_level * ones(1, length(X)), '-.', 'Color', 'k')
 
 xlabel('x')
+
+%% Phase potential in elliptic coordinate with two parameters: \lambda, \Lambda
+clc; clear
+
+lambda = 0.9; Lambda = 2;
+V = @(x) ( ((1/4) * (Lambda ^ 2) - lambda * (1 - lambda)) * (sn(x, lambda) .^ 2) ...
+	- Lambda * cn(x, lambda) ) ./ (dn(x, lambda) .^ 2);
+
+K = ellipk(sqrt(lambda));
+x = -(4*K):0.01:(4*K);
+
+plot(x, V(x)); grid on
+
+%% Rabi regime
+Lambda = 100;
+lambda = [0 0.25 0.5 0.8];
+linewidth = [2 2 1 1];
+linestyle = {'--'; '-'; '--'; '-'};
+
+% figure('Position', [100 100 500 225]); hold on
+
+% No legend
+figure('Position', [100 100 325 225]); hold on
+
+for i = 1:length(lambda)
+	V = @(x) ( ((1/4) * (Lambda ^ 2) - lambda(i) * (1 - lambda(i))) * (sn(x, lambda(i)) .^ 2) ...
+		- Lambda * cn(x, lambda(i)) ) ./ (dn(x, lambda(i)) .^ 2);
+	K = ellipk(sqrt(lambda(i)));
+	x = -(2*K):0.1:(2*K);
+	
+	plot(x, V(x), linestyle{i}, 'Color', 'black', 'LineWidth', linewidth(i));
+end
+
+h_legend = legend(...
+	  sprintf('\\lambda = %g', lambda(1)), ...
+	  sprintf('\\lambda = %g', lambda(2)), ...
+	  sprintf('\\lambda = %g', lambda(3)), ...
+	  sprintf('\\lambda = %g', lambda(4))  ...
+);
+
+% set(h_legend, 'location', 'northeastoutside');
+
+% Axis
+plot([-5, 5], [0 0], '--', 'Color', 'k');
+
+title(sprintf('\\Lambda = %g', Lambda));
+xlabel('x');
+ylabel('V_0(x)');
+
+%% Josephson regime
+
+Lambda = 0.4;
+lambda = [0 0.25 0.5 0.8];
+linewidth = [2 2 1 1];
+linestyle = {'--'; '-'; '--'; '-'};
+
+% figure('Position', [100 100 500 225]); hold on
+
+% No legend
+figure('Position', [100 100 325 225]); hold on
+
+for i = 1:length(lambda)
+	V = @(x) ( ((1/4) * (Lambda ^ 2) - lambda(i) * (1 - lambda(i))) * (sn(x, lambda(i)) .^ 2) ...
+		- Lambda * cn(x, lambda(i)) ) ./ (dn(x, lambda(i)) .^ 2);
+	K = ellipk(sqrt(lambda(i)));
+	x = -(2*K):0.1:(2*K);
+	
+	plot(x, V(x), linestyle{i}, 'Color', 'black', 'LineWidth', linewidth(i));
+end
+
+h_legend = legend(...
+	  sprintf('\\lambda = %g', lambda(1)), ...
+	  sprintf('\\lambda = %g', lambda(2)), ...
+	  sprintf('\\lambda = %g', lambda(3)), ...
+	  sprintf('\\lambda = %g', lambda(4))  ...
+);
+
+% set(h_legend, 'location', 'northeastoutside');
+
+% Axis
+plot([-5, 5], [0 0], '--', 'Color', 'k');
+
+title(sprintf('\\Lambda = %g', Lambda));
+xlabel('x');
+ylabel('V_0(x)');
+
+%% Fock regime
+
+Lambda = 0.01;
+lambda = [0 0.25 0.5 0.8];
+linewidth = [2 2 1 1];
+linestyle = {'--'; '-'; '--'; '-'};
+
+% figure('Position', [100 100 500 225]); hold on
+
+% No legend
+figure('Position', [100 100 325 225]); hold on
+
+for i = 1:length(lambda)
+	V = @(x) ( ((1/4) * (Lambda ^ 2) - lambda(i) * (1 - lambda(i))) * (sn(x, lambda(i)) .^ 2) ...
+		- Lambda * cn(x, lambda(i)) ) ./ (dn(x, lambda(i)) .^ 2);
+	K = ellipk(sqrt(lambda(i)));
+	x = -(2*K):0.1:(2*K);
+	
+	plot(x, V(x), linestyle{i}, 'Color', 'black', 'LineWidth', linewidth(i));
+end
+
+h_legend = legend(...
+	  sprintf('\\lambda = %g', lambda(1)), ...
+	  sprintf('\\lambda = %g', lambda(2)), ...
+	  sprintf('\\lambda = %g', lambda(3)), ...
+	  sprintf('\\lambda = %g', lambda(4))  ...
+);
+
+% set(h_legend, 'location', 'northeastoutside');
+
+% Axis
+plot([-5, 5], [0 0], '--', 'Color', 'k');
+
+title(sprintf('\\Lambda = %g', Lambda));
+xlabel('x');
+ylabel('V_0(x)');
+
+
+
+
