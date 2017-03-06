14bce1b0de25a054506c730aae44c4e99eb8bac4
Mikhail Lebedev <m.lebedev@lmc.ifmo.ru>
2017-03-06 15:37:48 +0300

    Restuctured, picture with phase on dissipation was improved

--- phase_transition/preparing_paper_dissipation.m
+++ phase_transition/preparing_paper_dissipation.m
@@ -1,5 +1,3 @@
-%% ?????????? ??? ??????? ??? exp(-2 * kappa * t)
-
 %% Libs
 addpath ../finding_stationary_modes/
 
@@ -18,7 +16,7 @@ clc; clear
 % \alpha \to \alpha \exp(-\kappa t)
 % \beta \to \beta \exp(-\kappa t)
 
-alpha = 1; beta = 0.9; B = 1; R = 1;
+alpha = 1; beta = 0.5; B = 0.1; R = 1;
 lambda_1 = beta / alpha;
 Lambda_1 = B / (alpha * R); % s = R (?)
 
@@ -34,7 +32,7 @@ dS = @(t, S, alpha, beta, kappa) [
 
 % S_x^2 + S_y^2 + S_z^2 = S_0^2 = R^2
 % Initial condition
-Sz0 = 0; Sx0 = 0.62;
+Sz0 = 0; Sx0 = 0;
 Sy0 = sqrt(R^2 - Sx0^2 - Sz0^2);
 
 % Phase
@@ -42,12 +40,12 @@ Sy0 = sqrt(R^2 - Sx0^2 - Sz0^2);
 
 % (I) Phase with dissipation
 % Parameter fo the dissipation
-kappa = 0.004;
-T_fin = 300;
+kappa = 0.05;
+T_fin = 70;
 
 % RK parameters
 dS_kappa = @(t, S) dS(t, S, alpha, beta, kappa);
-tspan = [0 T_fin]; S0 = [Sz0; Sx0; Sy0]; N = 2^14;
+tspan = [0 T_fin]; S0 = [Sz0; Sx0; Sy0]; N = 2^12;
 [T, S_dissip] = RK4(dS_kappa, tspan, S0, N);
 phase_dissip = atan(S_dissip(:, 3) ./ S_dissip(:, 2));
 
@@ -63,7 +61,7 @@ phase = atan(S_barrier(:, 3) ./ S_barrier(:, 2));
 plot(T, phase, 'Color', 'blue');
 
 % (III) Phase in a new regime without dissipation
-t_new_regime = 250; % Look at the figure
+t_new_regime = 65; % Look at the figure
 new_alpha = alpha_dissip(t_new_regime, alpha, kappa);
 new_beta  = beta_dissip(t_new_regime, beta, kappa);
 
@@ -87,35 +85,46 @@ plot(T, phase, 'Color', 'red');
 
 % Legend
 legend( ...
-	sprintf('\\kappa = %g, \\lambda = %g, \\Lambda = %g', kappa, lambda_1, Lambda_1), ...
-	sprintf('\\kappa = 0, \\lambda = %g, \\Lambda = %g', lambda_1, Lambda_1), ...
-	sprintf('\\kappa = 0, \\lambda = %g, \\Lambda = %g', lambda_2, Lambda_2))
+	sprintf('Dissipative: \\lambda = %g, \\Lambda(0) = %g', lambda_1, Lambda_1), ...
+	sprintf('Non-dissipative: \\lambda = %g, \\Lambda = %g', lambda_1, Lambda_1), ...
+	sprintf('Non-dissipative: \\lambda = %g, \\Lambda = %g', lambda_2, Lambda_2))
 
 xlabel('t'); ylabel('\phi')
 
 %% Potential transformation
-V = @(x) alpha * (R^2) * ( ((1/4) * (Lambda_1 ^ 2) - lambda_1 * (1 - lambda_1)) * (sn(x, lambda_1) .^ 2) ...
+figure('Position', [100 100 325 225]); hold on
+
+K = ellipk(sqrt(lambda_2));
+x = -(1.5*K):0.01:(1.5*K);
+
+Lambda_crit = 2 * lambda_1;
+
+alpha_crit = B / (Lambda_crit * R);
+V = @(x) alpha_crit * ( ((1/4) * (Lambda_crit ^ 2) - lambda_1 * (1 - lambda_1)) * (sn(x, lambda_1) .^ 2) ...
+	- Lambda_crit * cn(x, lambda_1) ) ./ (dn(x, lambda_1) .^ 2);
+
+plot(x, V(x), 'Color', 'black', 'LineWidth', 2);
+
+V = @(x) alpha * ( ((1/4) * (Lambda_1 ^ 2) - lambda_1 * (1 - lambda_1)) * (sn(x, lambda_1) .^ 2) ...
 	- Lambda_1 * cn(x, lambda_1) ) ./ (dn(x, lambda_1) .^ 2);
 
 K = ellipk(sqrt(lambda_1));
-x = -(K):0.01:(K);
+x = -(1.5*K):0.01:(1.5*K);
 
-figure('Position', [100 100 325 225]); hold on
-plot(x, V(x), 'Color', 'black', 'LineWidth', 2)
+plot(x, V(x), 'Color', 'blue')
 
-V = @(x) new_alpha * (R^2) * ( ((1/4) * (Lambda_2 ^ 2) - lambda_2 * (1 - lambda_2)) * (sn(x, lambda_2) .^ 2) ...
+V = @(x) new_alpha * ( ((1/4) * (Lambda_2 ^ 2) - lambda_2 * (1 - lambda_2)) * (sn(x, lambda_2) .^ 2) ...
 	- Lambda_2 * cn(x, lambda_2) ) ./ (dn(x, lambda_2) .^ 2);
-
-K = ellipk(sqrt(lambda_2));
-x = -(K):0.01:(K);
 
 plot(x, V(x), 'Color', 'red')
 
+
 legend(...
+	sprintf('\\lambda = %g, \\Lambda = %g', lambda_1, Lambda_crit), ...
 	sprintf('\\lambda = %g, \\Lambda = %g', lambda_1, Lambda_1), ...
 	sprintf('\\lambda = %g, \\Lambda = %g', lambda_1, Lambda_2))
 
-xlabel('x'); ylabel('V(x)')
+xlabel('z'); ylabel('V(z)')
 
 %% Adiabatic approximation
 omega = @(t) sqrt((2*alpha_dissip(t, alpha, kappa) - 2*beta_dissip(t, beta, kappa) + B) ...
@@ -133,7 +142,7 @@ plot(T_1, phase_adiabatic(T_1), 'Color', 'green', 'LineWidth', 2);
 %% Compare equivalent systems and their solutions
 clc; clear
 
-alpha = 1; beta = 0.9; B = 1; R = 1; kappa = 0.004; T_fin = 300; t_new_regime = 200;
+alpha = 1; beta = 0.5; B = 1; R = 1; kappa = 0.004; T_fin = 300; t_new_regime = 200;
 
 alpha_dissip = @(t, alpha, kappa) alpha * exp(-kappa * t);
 beta_dissip  = @(t,  beta, kappa) beta  * exp(-kappa * t);
@@ -236,7 +245,7 @@ plot(T1, solution(T1), 'magenta');
 %% Phase portrait on the sphere
 clc; clear
 
-alpha = 1; beta = 0.9; B = 0.18;
+alpha = 1; beta = 0.5; B = 0.1;
 kappa = 0.0;
 
 df = @(t, f) [2*beta * f(2) * f(3) * exp(-2*kappa*t) - B*f(3);
@@ -267,6 +276,40 @@ for S30 = -0.3:0.05:0.3
 
 	plot3(F(1:5:end, 1), F(1:5:end, 2), F(1:5:end, 3));
 end
+
+%% Temperature (thermon period) and dissipation
+clc; clear;
+
+beta = 0.5; alpha = 1; B = 0.1; R = 1;
+lambda = beta / alpha;
+
+kappa = 0.05;
+
+alpha_dissip = @(t) alpha * exp(-2 * kappa * t);
+beta_dissip  = @(t) beta  * exp(-2 * kappa * t);
+Lambda_dissip = @(t) B / (alpha_dissip(t) * R);
+
+T = [];
+
+t = 0; tstep = 0.01;
+while true
+	Lambda = Lambda_dissip(t);
+	
+	if Lambda > (2 * lambda)
+		break
+	end
+	
+	T = [T sqrt(lambda * (1 - lambda) + (2*lambda - 1) * Lambda / 2 - Lambda^2 / 4)];
+	t = t + tstep;
+end
+
+time = 0:tstep:t;
+
+figure('Position', [100 100 325 225]); hold on;
+% plot(kappa * time, T, 'black');
+plot(time, 1 ./ [T 0], 'black')
+% xlabel('t'); ylabel('T / T_0')
+xlabel('t'); ylabel('\tau_p / \tau_0')
 
 
 
