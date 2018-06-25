44bb7250f9129ddd92417d06d0f6a1612096cc1e
Mikhail Lebedev <st_w@rambler.ru>
2018-06-26 02:37:24 +0300

    diagram plotter was added and tested

--- kolesnikova/test_diagram.m
+++ kolesnikova/test_diagram.m
@@ -1,5 +1,13 @@
 %% Test (u(0), u_x(0)) - diagrams plotting
+% Compare with Kolesnikova bachelor work
+omega = 0; alpha = 1; params = [omega alpha];
+diagram(params)
+
+omega = 0; alpha = 8; params = [omega alpha];
+diagram(params)
+
 % Compare with G. L. Alfimov and D. A. Zezyulin
 %   Nonlinear modes for the Gross-Pitaevskii equation -- a demonstrative computation approach
 
-% TODO
+oemga = 0; alpha = 0; params = [omega, alpha];
+diagram(params)
