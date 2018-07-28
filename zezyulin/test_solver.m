ef7187152dae9ce174b77cf5cd4f0660e6ca0a78
Mikhail Lebedev <st_w@rambler.ru>
2018-07-28 17:22:22 +0300

    modify tests

--- zezyulin/test_solver.m
+++ zezyulin/test_solver.m
@@ -1,12 +1,12 @@
 %% Test MEX solver for the Kolesnikova task
 clc; clear
 
-omega = 1; alpha = 1; params = [omega alpha];
-ode = @(x, u) [u(2), -(omega - (x ^ 2)) * u(1) - (1 + alpha * (x ^ 2)) * (u(1) ^ 3)];
+mu = -2; omega = sqrt(2); A = 2; params = [mu omega A];
+ode = @(x, u) [u(2), -(mu - 0.5 * ((omega ^ 2) * (x ^ 2))) * u(1) - (1 + A * (tanh(x) ^ 2)) * (u(1) ^ 3)];
 
 xspan = [0 1];
 u0 = [1, 1];
-N = 1024;
+N = 10;
 
 %% Run MATLAB implementation of RK4
 [X, U] = RK4(ode, xspan, u0, N);
